d4ba2388d3c08c737901c551d0189a19e4b9dd1d
ArnabNayan <arnabnayan5@gmail.com>
2023-05-03 18:04:26 +0600

    registration file validation done

--- src/Components/Layout/Login/Login.jsx
+++ src/Components/Layout/Login/Login.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 
 const Login = () => {
+
+
+  const handleLogin=event=>{
+    event.preventDefault()
+    const form=event.target
+    const email=form.email.value;
+    const password=form.password.value;
+    console.log(email,password)
+   
+
     
- 
+  }
+
     return (
       <div className="hero min-h-screen  bg-base-200 mt-5">
       <div className="hero-content flex-col">
@@ -15,25 +26,27 @@ const Login = () => {
           <h1 className="text-5xl font-bold">Login now!</h1>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mt-5">
-          <form className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email"name="email" className="input input-bordered"required />
+              <input type="email" placeholder="email"name="email" className="input input-bordered"  required />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" name="password"className="input input-bordered"required />
+              <input type="password" placeholder="password" name="password"className="input input-bordered" required />
             </div>
+       
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
           </form>
+     
           <p>New to Food Master?<Link to="/register" className="label-text-alt link link-hover text-lg text-blue-500">Register</Link></p>
-       
+        
         
         </div>
       </div>
--- src/Components/Layout/Register/Register.jsx
+++ src/Components/Layout/Register/Register.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const [error,setError]=useState('')
+
+    const handleRegister=event=>{
+        event.preventDefault()
+        const form=event.target;
+        const name=form.name.value;
+        const email=form.email.value;
+        const photo=form.photo.value;
+        const password=form.password.value;
+        console.log(name,email,photo,password)
+        setError('')
+
+        if(password.length<6){
+            setError('Your password must be at least 6 characters')
+            return
+        }
+
+    }
+    
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col">
@@ -9,7 +28,7 @@ const Register = () => {
             <h1 className="text-5xl font-bold">Please Register!!!!</h1>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleRegister} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
@@ -26,7 +45,7 @@ const Register = () => {
                 <label className="label">
                   <span className="label-text">Photo URL</span>
                 </label>
-                <input type="text" placeholder="Photo URL"name="photo" className="input input-bordered"required />
+                <input type="text" placeholder="Photo URL"name="photo"className="input input-bordered"required />
               </div>
               <div className="form-control">
                 <label className="label">
@@ -41,7 +60,9 @@ const Register = () => {
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Register</button>
               </div>
+             
             </form>
+            <p className='text-red-500 text-lg'>{error}</p>
           </div>
         </div>
       </div>
