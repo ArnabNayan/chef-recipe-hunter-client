ec5f6398db7d3bad8673fedc0303b0bbce43c155
ArnabNayan <arnabnayan5@gmail.com>
2023-05-04 21:45:08 +0600

    active link done

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,14 +2,105 @@ import React, { useEffect, useState } from 'react';
 import Login from '../Layout/Login/Login';
 
 const Blog = () => {
-   
-  
 
-    
     return (
-        <div>
-         <h2>This is blog</h2>
-        </div>
+        <>
+            <h1 className='text-center font-extrabold text-5xl mt-6 text-indigo-600'>Some Questions and Answers</h1>
+            <div className='bg-sky-200'>
+                <h1 className='font-bold text-2xl mt-8 lg:ms-8'>1.Tell us the differences between uncontrolled and controlled components.</h1>
+
+                <div className="overflow-x-auto mt-5">
+                    <table className="table w-3/4 mt-7">
+                        {/* head*/}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Uncontrolled components</th>
+                                <th>Controlled components</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <th>1</th>
+                                <td>Managed by components own internal state</td>
+                                <td>Managed by React state</td>
+
+                            </tr>
+                            {/* row 2 */}
+                            <tr className="active">
+                                <th>2</th>
+                                <td>Component updates own internal state on user interaction</td>
+                                <td>Parent component updates state on user interaction</td>
+
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th>3</th>
+                                <td>Generally slower as theres more state management</td>
+                                <td>Generally faster as theres less state management</td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <h1 className='font-bold text-2xl mt-8 lg:ms-8'>2.How to validate React props using PropTypes</h1>
+                <p className='mt-5 lg:ms-8 font-semibold'>Props and PropTypes are important mechanisms for passing read-only attributes between React components.
+
+                    We can use React props, short for properties, to send data from one component to another. If a component receives the wrong type of props, it can cause bugs and unexpected errors in your app.
+
+                    Since JavaScript doesn’t have a built-in type checking solution, many developers use extensions like TypeScript and Flow. However, React has an internal mechanism for props validation called PropTypes. In this article, we’ll learn how to validate props with React PropTypes.</p>
+
+                <h1 className='font-bold text-2xl mt-8 lg:ms-8'>3.Tell us the difference between nodejs and express js.</h1>
+
+                <div className="overflow-x-auto mt-5">
+                    <table className="table w-3/4 mt-7">
+                        {/* head*/}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>nodejs</th>
+                                <th>expressjs</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <th>1</th>
+                                <td> It is used to build server-side, input-output, event-driven apps.</td>
+                                <td>	It is used to build web-apps using approaches and principles of Node.js.</td>
+
+                            </tr>
+                            {/* row 2 */}
+                            <tr className="active">
+                                <th>2</th>
+                                <td>Run-time platform or environment designed for server-side execution of JavaScript.</td>
+                                <td>Framework based on Node.js.</td>
+
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th>3</th>
+                                <td>Doesn’t use such a provision.</td>
+                                <td>Uses middleware for the arrangement of functions systematically server-side.</td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <h1 className='font-bold text-2xl mt-8 lg:ms-8'>4.What is a custom hook, and why will you create a custom hook?</h1>
+                <p className='mt-5 lg:ms-8 font-semibold'>Custom React JS hooks are reusable functions that a React JS software developer can use to add special and unique functionality to the React applications. Usually, if there is a requirement to add a feature, one can install a third-party library and solve the problem. But what if there is no such library with hooks that can be used? This problem is solved by using custom React JS hooks.
+
+                    A custom hook is a special JavaScript function whose name starts with ‘use’ and can be used to call other hooks. Let’s take a look at some major differences between a custom React JS hook and React JS components:
+
+                    A custom hook does not require a specific signature.
+                    A software developer can choose what argument the custom hook has and what should the argument return.
+                    A custom hook always starts with the name ‘use’</p>
+            </div>
+        </>
     );
 };
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
 
 
@@ -9,6 +9,9 @@ const Header = () => {
        logOut()
        .then(result=>{})
        .catch(error=>console.error(error))
+
+       const location=useLocation();
+       const activeClass = "text-blue-500 font-bold";
     return (
        
         <nav className=" navbar bg-slate-200 font-bold text-amber-700  flex flex-col sm:flex-row justify-between items-center px-4 py-2">
@@ -17,9 +20,21 @@ const Header = () => {
             </div>
 
             <div className='text-xl text-gray-700 lg:me-20 sm:text-center flex flex-col sm:flex-row gap-4 sm:gap-10 '>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/Blog">Blog</Link>
+                <Link to="/" className={
+            location.pathname === "/"
+              ? `${activeClass} underline`
+              : "no-underline"
+          }>Home</Link>
+                <Link to="/login"  className={
+            location.pathname === "/login"
+              ? `${activeClass} underline`
+              : "no-underline"
+          }>Login</Link>
+                <Link to="/Blog"className={
+            location.pathname === "/Blog"
+              ? `${activeClass} underline`
+              : "no-underline"
+          }>Blog</Link>
                 {user &&  <><button onClick={handleLogOut}>Signout</button><img className="w-14 rounded-full lg:ms-36" src="/images/player-1.png" /></>}
             </div>
         </nav>
